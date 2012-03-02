a7e96c6159676d884825b8feb7c7def1c8690e9f
KiM <KiM@KiMs-Dator>
2012-03-03 00:25:29 +0100

    First from Kim

--- src/com/CMDbox/CMDbox.java
+++ src/com/CMDbox/CMDbox.java
@@ -1,5 +1,28 @@
 package com.CMDbox;
 
-public class CMDbox {
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class CMDbox extends JavaPlugin {
+	public CMDbox plugin;
+	
+	public void onDisable(){
+		
+	}
+	
+	public void onEnable(){
+		PluginManager pm = getServer().getPluginManager();
+		
+		getCommands(this);
+		PluginConfig(this);
+	}
+	
+	public void getCommands(CMDbox CMDbox){
+		
+	}
+	
+	public void PluginConfig(CMDbox CMDbox){
+		this.getConfig().addDefault("blah", "blah");
+		this.saveConfig();
+	}
 }
