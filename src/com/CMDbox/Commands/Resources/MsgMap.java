5d5e76576dd4a849a489bf6688eb101160fb68f7
KiM <KiM@KiMs-Dator>
2012-03-03 23:05:38 +0100

    HashMap Tut

---
+++ src/com/CMDbox/Commands/Resources/MsgMap.java
@@ -0,0 +1,16 @@
+package com.CMDbox.Commands.Resources;
+
+import java.util.HashMap;
+
+public class MsgMap {
+	public static HashMap<String, Integer> msg = new HashMap<String, Integer>();
+	
+//	public void setValue (){
+//		msg.put("Chest", 1);
+//	}
+//	
+//	public void getvalue(){
+//		int name = msg.get("Chest2");
+//		System.out.println(name);
+//	}
+}
