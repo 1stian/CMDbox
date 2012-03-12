b9038c5929419d2511e61d5b18c31db2fbeb0810
Stian <Stian@10.0.0.58>
2012-03-12 08:22:21 +0100

    Working on home. Taking a break. xD. more coding soon to finish the home command!

--- src/com/CMDbox/Commands/CommandHome.java
+++ src/com/CMDbox/Commands/CommandHome.java
@@ -1,11 +1,15 @@
 package com.CMDbox.Commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.CMDbox.CMDbox;
 import com.CMDbox.Config.DefaultConfig;
+import com.CMDbox.Config.HomesFile;
 
 public class CommandHome implements CommandExecutor {
 	public CommandHome(CMDbox cmDbox) {
@@ -21,6 +25,17 @@ public class CommandHome implements CommandExecutor {
 					}else if(args.length > 1){
 						cs.sendMessage("Too many arguments!");
 						cs.sendMessage("Usage: /home homeName");
+					}else{
+						String name = cs.getName();
+						if(HomesFile.HomesConfig.contains(name + "." + args[0])){
+							String cWorld = HomesFile.HomesConfig.getString(name + "."+ args[0] + ".world");
+					    	int x = HomesFile.HomesConfig.getInt(name + "."+ args[0] + ".x");
+					    	int y = HomesFile.HomesConfig.getInt(name + "."+ args[0] + ".y");
+					    	int z = HomesFile.HomesConfig.getInt(name + "."+ args[0] + ".z");
+					    	Player player = (Player) cs;
+					    	Location homeloc = new Location(Bukkit.getWorld(cWorld), x,y,z);
+					    	player.teleport(homeloc);
+						}
 					}
 				}else{
 					
